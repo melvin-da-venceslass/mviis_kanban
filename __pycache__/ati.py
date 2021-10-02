990b06aef6c01d902afa1b09f0e01f4202228204
melvin <mechatmick@gmail.com>
2021-10-02 23:07:35 +0530

    Updated Version

--- __pycache__/ati.py
+++ __pycache__/ati.py
@@ -1,6 +1,6 @@
 import requests
 
-for each in range(60,101):
+for each in range(101):
     mm ="ID_"+str(each)
     x = requests.post(url="https://mviis-kanban-app.herokuapp.com/store",json={"uniqname":mm})
     print(x.json())
